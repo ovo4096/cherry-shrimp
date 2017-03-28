596b51676f866c45c11110043b458f2256202f1a
ovo4096 <ovo4096@gmail.com>
2017-03-29 01:15:54 +0800

    Add Electron's VueJS build environment

--- main.js
+++ main.js
@@ -1,55 +1,57 @@
 const { app, BrowserWindow } = require('electron')
-const path = require('path')
 const url = require('url')
+const debug = /--debug/.test(process.argv[2])
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let win
 
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+function createMainWindow () {
+  function createWindow (url) {
+    win = new BrowserWindow({
+      width: 800,
+      height: 600
+      // webPreferences: {
+      //   nodeIntegration: false
+      // }
+    })
 
-  // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+    win.loadURL(url)
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
+    win.webContents.openDevTools()
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
-  })
+    win.on('closed', () => {
+      win = null
+    })
+  }
+
+  if (debug) {
+    const { ready } = require('./build/dev-server')
+    const config = require('./config')
+
+    let port = process.env.PORT || config.dev.port
+    let urlPath = url.format({
+      hostname: 'localhost',
+      protocol: 'http:',
+      port
+    })
+
+    ready.then(() => {
+      createWindow(urlPath)
+    })
+  } else {
+    require('./build/build')
+  }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createMainWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    createMainWindow(url)
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
