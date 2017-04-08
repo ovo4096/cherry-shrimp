0bac4819dc984e27d2285b7f93429bcbb47c878e
ovo4096 <ovo4096@gmail.com>
2017-04-08 16:33:02 +0800

    Add header searchbar

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,23 +1,18 @@
 <template>
-  <!-- The drawer is always open in large screens. The header is always shown,
-                      even in small screens. -->
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-    <header class="mdl-layout__header">
+    <header class="mdl-layout__header" ref="header">
       <div class="mdl-layout__header-row">
         <span class="mdl-layout__title">Simple 2</span>
         <div class="mdl-layout-spacer"></div>
-        <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right">
-          <label class="mdl-button mdl-js-button mdl-button--icon"
-                 for="fixed-header-drawer-exp">
-            <i class="material-icons">search</i>
-          </label>
-          <div class="mdl-textfield__expandable-holder">
-            <input class="mdl-textfield__input"
-                   type="text"
-                   name="sample"
-                   id="fixed-header-drawer-exp">
-          </div>
+        <button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" @click="enterSearch">
+          <i class="material-icons">search</i>
+        </button>
+      </div>
+      <div class="mdl-layout__header-search">
+        <div class="mdl-layout__header-search-back" @click="leaveSearch">
+          <i class="material-icons">arrow_back</i>
         </div>
+        <input type="text" placeholder="Search" class="mdl-layout__header-search-textfield">
       </div>
     </header>
     <div class="mdl-layout__drawer drawer">
@@ -36,7 +31,6 @@
     <main class="mdl-layout__content">
       <div class="page-content">
         <!-- Your content goes here -->
-        <search></search>
         <div class="mdl-grid">
           <div class="mdl-cell mdl-cell--1-col">1</div>
           <div class="mdl-cell mdl-cell--1-col">1</div>
@@ -68,20 +62,28 @@
         </div>
       </div>
     </main>
+    <div class="mdl-layout__obfuscator" ref="obfuscator" @click="leaveSearch"></div>
   </div>
 </template>
 
 <script>
-import Search from '@/components/Search'
-
 export default {
   name: 'app',
-  components: { Search },
-  mounted () { }
+  mounted () { },
+  methods: {
+    enterSearch () {
+      this.$refs.header.classList.toggle('is-visible-search')
+      this.$refs.obfuscator.classList.toggle('is-visible')
+    },
+    leaveSearch () {
+      this.$refs.header.classList.toggle('is-visible-search')
+      this.$refs.obfuscator.classList.toggle('is-visible')
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .drawer {
   border: none;
 }
@@ -89,5 +91,82 @@ export default {
 .account-header {
   height: 150px;
   background: url('./assets/account-header-bg.png');
+}
+</style>
+
+<style lang="scss" scoped>
+@import "~material-design-lite/src/_variables";
+@import "~material-design-lite/src/_mixins";
+
+.mdl-layout__header.is-visible-search {
+  z-index: 5;
+
+  .mdl-layout__header-row {
+    display: none;
+  }
+
+  .mdl-layout__header-search {
+    display: flex;
+  }
+}
+
+.mdl-layout__header-search {
+  display: none;
+  flex-grow: 1;
+  margin: ($layout-mobile-header-height - $layout-drawer-button-desktop-size) 12px;
+  border-radius: 2px;
+  box-sizing: border-box;
+  min-height: $layout-desktop-header-height - ($layout-mobile-header-height - $layout-drawer-button-desktop-size) * 2;
+  background: #fff;
+  z-index: 4;
+  color: rgba(0, 0, 0, 0.5);
+  overflow: hidden;
+  position: relative;
+
+  @include shadow-2dp();
+
+  @media screen and (max-width: $layout-screen-size-threshold) {
+    margin: 8px;
+    min-height: $layout-mobile-header-height - 16px;
+  }
+}
+
+.mdl-layout__header-search-back {
+    overflow: hidden;
+    text-align: center;
+    cursor: pointer;
+    height: $layout-drawer-button-desktop-size;
+    width: $layout-drawer-button-desktop-size;
+    font-size: 26px;
+    line-height: $layout-mobile-header-height;
+    font-family: Helvetica, Arial, sans-serif;
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    @media screen and (max-width: $layout-screen-size-threshold) {
+      height: $layout-mobile-header-height - 16px;
+      width: $layout-mobile-header-height - 16px;
+      line-height: $layout-mobile-header-height - 8px;
+    }
+}
+
+.mdl-layout__header-search-textfield {
+  border: none;
+  padding: 0 ($layout-header-desktop-indent - 12px) 0 ($layout-header-desktop-baseline - 12px);
+  flex-grow: 1;
+  font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+  font-size: 20px;
+  letter-spacing: 0.02em;
+  font-weight: 400;
+  box-sizing: border-box;
+
+  @media screen and (max-width: $layout-screen-size-threshold) {
+    padding: 0 ($layout-header-mobile-indent - 8px) 0 ($layout-header-mobile-baseline - 8px);
+  }
+}
+
+.mdl-layout__header-search-textfield:focus {
+  outline: none;
 }
 </style>
