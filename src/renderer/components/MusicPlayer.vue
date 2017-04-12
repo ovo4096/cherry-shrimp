e75dc58a25f1f49602fc5303806893eb44801ec1
ovo4096 <ovo4096@gmail.com>
2017-04-12 20:00:11 +0800

    Add playlist layout

--- src/renderer/components/MusicPlayer.vue
+++ src/renderer/components/MusicPlayer.vue
@@ -1,52 +1,106 @@
 <template>
-  <div class="music-player">
-    <div class="player-progress">
-      <div class="progress">
-        <input class="mdl-slider mdl-js-slider" type="range" min="0" max="100" value="0" tabindex="0">
+  <div>
+    <div class="playlist" ref="playlist">
+      <div class="container-wrapper">
+        <div class="container">
+          <table class="mdl-data-table mdl-js-data-table">
+            <thead>
+              <tr>
+                <th></th>
+                <th class="mdl-data-table__cell--non-numeric">SONG</th>
+                <th>DURATION</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <div class="music-cover"></div>
+                </td>
+                <td class="mdl-data-table__cell--non-numeric">
+                  <div class="music-info">
+                    <div class="name">Futurism Love (feat. Miku Hatsune)</div>
+                    <div class="auth"><a href="#">BAKAEDITZ</a> - <a href="#">Life in the 2k12</a></div>
+                  </div>
+                </td>
+                <td>3:54</td>
+                <td>
+                  <button class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored">
+                    <i class="material-icons">favorite</i>
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <div class="music-cover"></div>
+                </td>
+                <td class="mdl-data-table__cell--non-numeric">
+                  <div class="music-info">
+                    <div class="name">Deneb</div>
+                    <div class="auth"><a href="#">REDALiCE</a> - <a href="#">Emperor</a></div>
+                  </div>
+                </td>
+                <td>3:54</td>
+                <td>
+                  <button class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored">
+                    <i class="material-icons">favorite</i>
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
-    <div class="music">
-      <div class="music-cover"><img src="../assets/logo.png" alt="logo"></div>
-      <div class="music-intro">
-        <div class="music-info">
-          <div class="name">Futurism Love (feat. Miku Hatsune)</div>
-          <div class="auth"><a href="#">BAKAEDITZ</a> - <a href="#">Life in the 2k12</a></div>
-        </div>
-        <div class="music-action">
-          <button class="mdl-button mdl-js-button mdl-button--icon">
-            <i class="material-icons">favorite</i>
-          </button>
-        </div>
-      </div>
-    </div>
-    <div class="player-control">
-      <button class="mdl-button mdl-js-button mdl-button--icon">
-        <i class="material-icons">repeat</i>
-      </button>
-      <button class="mdl-button mdl-js-button mdl-button--icon">
-        <i class="material-icons">skip_previous</i>
-      </button>
-      <button class="mdl-button mdl-js-button mdl-button--play mdl-button--colored">
-        <i class="material-icons">play_circle_filled</i>
-      </button>
-      <button class="mdl-button mdl-js-button mdl-button--icon">
-        <i class="material-icons">skip_next</i>
-      </button>
-      <button class="mdl-button mdl-js-button mdl-button--icon">
-        <i class="material-icons">shuffle</i>
-      </button>
-    </div>
-    <div class="player-action">
-      <div class="audio-volume" ref="audioVolume" @mouseleave="hideAudioVolume" @mouseenter="showAudioVolume">
-        <i class="material-icons volume-down">volume_down</i>
-        <div class="audio-volume-slider">
+    <div class="music-player">
+      <div class="player-progress">
+        <div class="progress">
           <input class="mdl-slider mdl-js-slider" type="range" min="0" max="100" value="0" tabindex="0">
         </div>
-        <i class="material-icons volume-up">volume_up</i>
       </div>
-      <button class="mdl-button mdl-js-button mdl-button--icon button-queue-music">
-        <i class="material-icons">queue_music</i>
-      </button>
+      <div class="music">
+        <div class="music-cover"><img src="../assets/logo.png" alt="logo"></div>
+        <div class="music-intro">
+          <div class="music-info">
+            <div class="name">Futurism Love (feat. Miku Hatsune)</div>
+            <div class="auth"><a href="#">BAKAEDITZ</a> - <a href="#">Life in the 2k12</a></div>
+          </div>
+          <div class="music-action">
+            <button class="mdl-button mdl-js-button mdl-button--icon">
+              <i class="material-icons">favorite</i>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="player-control">
+        <button class="mdl-button mdl-js-button mdl-button--icon">
+          <i class="material-icons">repeat</i>
+        </button>
+        <button class="mdl-button mdl-js-button mdl-button--icon">
+          <i class="material-icons">skip_previous</i>
+        </button>
+        <button class="mdl-button mdl-js-button mdl-button--play mdl-button--colored">
+          <i class="material-icons">play_circle_filled</i>
+        </button>
+        <button class="mdl-button mdl-js-button mdl-button--icon">
+          <i class="material-icons">skip_next</i>
+        </button>
+        <button class="mdl-button mdl-js-button mdl-button--icon">
+          <i class="material-icons">shuffle</i>
+        </button>
+      </div>
+      <div class="player-action">
+        <div class="audio-volume" ref="audioVolume" @mouseleave="hideAudioVolume" @mouseenter="showAudioVolume">
+          <i class="material-icons volume-down">volume_down</i>
+          <div class="audio-volume-slider">
+            <input class="mdl-slider mdl-js-slider" type="range" min="0" max="100" value="0" tabindex="0">
+          </div>
+          <i class="material-icons volume-up">volume_up</i>
+        </div>
+        <button class="mdl-button mdl-js-button mdl-button--icon button-queue-music" ref="togglePlaylistButton" @click="togglePlaylist">
+          <i class="material-icons">queue_music</i>
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -59,6 +113,10 @@ export default {
     },
     hideAudioVolume () {
       this.$refs.audioVolume.classList.remove('show')
+    },
+    togglePlaylist () {
+      this.$refs.playlist.classList.toggle('show')
+      this.$refs.togglePlaylistButton.classList.toggle('mdl-button--colored')
     }
   }
 }
@@ -66,7 +124,6 @@ export default {
 
 <style lang="scss">
 @import "../assets/_variables";
-
 .player-progress {
   .progress {
     > .mdl-slider__container {
@@ -82,7 +139,7 @@ export default {
     }
 
     .mdl-slider__background-upper {
-      background: #e6e6e6;
+      background: none;
     }
 
     .mdl-slider.is-upgraded.is-lowest-value + .mdl-slider__background-flex > .mdl-slider__background-upper {
@@ -112,15 +169,106 @@ export default {
     }
   }
 }
+
+.music-player:hover {
+  .mdl-slider__background-upper {
+    background: #e6e6e6;
+  }
+}
 </style>
 
 <style lang="scss" scoped>
 @import "../assets/_variables";
 @import "~material-design-lite/src/_mixins";
 
-$button-play-fab-font-size: 24px + 26px !default;
-$button-play-icon-size: 32px + 26px !default;
-$button-play-icon-size-mini: 24px + 26px !default;
+$button-play-fab-font-size: 24px + 26px + 17px !default;
+$button-play-icon-size: 32px + 26px + 17px !default;
+$button-play-icon-size-mini: 24px + 26px + 17px !default;
+
+.playlist {
+  position: fixed;
+  top: auto;
+  left: auto;
+  right: 16px;
+  bottom: 110px;
+  background: #fff;
+
+  max-width: 732px;
+  border-radius:2px;
+  z-index: 2;
+  transform-origin: right bottom 0px;
+  transform: scale(0);
+  transition: all .3s ease;
+
+  @include shadow-2dp();
+
+  &.show {
+    transform: scale(1);
+  }
+
+  .music-cover {
+    width: 40px;
+    height: 40px;
+    background: #333;
+  }
+
+  .music-info {
+    overflow: hidden;
+    max-width: 400px;
+    .name {
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+    }
+    .auth {
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+      a, a:hover, a:focus {
+        color: #616161;
+        text-decoration: none;
+      }
+
+      a:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+
+  .container-wrapper {
+    padding: 2px 0;
+    overflow: hidden;
+  }
+
+  .container {
+    max-height: calc(100vh - 110px - 112px - 16px);
+    overflow: auto;
+  }
+
+  .mdl-data-table {
+    border: none;
+  }
+
+  .mdl-data-table td {
+    border-bottom: none;
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    width: 0;
+    height: 0;
+    box-sizing: border-box;
+    right: 24px;
+    border: 8px solid white;
+    z-index: -1;
+    transform-origin: 0 0;
+    -webkit-transform-origin: 0 0;
+    transform: rotate(-45deg);
+    -webkit-transform: rotate(-45deg);
+    box-shadow: -2px 2px 2px 0px rgba(0, 0, 0, 0.24);
+  }
+}
 
 .player-progress {
   position: absolute;
