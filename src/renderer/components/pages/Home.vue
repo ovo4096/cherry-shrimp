9ac5c3c2853ec7ea2226cd831a8ae0e3ef658a9e
ovo4096 <ovo4096@gmail.com>
2017-04-16 03:35:37 +0800

    Add music set card layout

--- src/renderer/components/pages/Home.vue
+++ src/renderer/components/pages/Home.vue
@@ -9,123 +9,18 @@
       <div class="mdl-grid">
         <div class="mdl-cell mdl-cell--12-col" ref="scrollbars">
           <div class="mdl-card-group iscroll-wrapper">
-            <div class="mdl-card mdl-shadow--2dp">
-              <div class="mdl-card__title mdl-card--expand">
-                <h2 class="mdl-card__title-text">
+            <div v-for="i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]" class="music-set-card">
+              <div class="mdl-card">
+                <div class="mdl-card__title mdl-card--expand">
+                  <h2 class="mdl-card__title-text">
                     Today's Best Music
                   </h2>
-              </div>
-              <div class="mdl-card__title">
-                <div class="mdl-card__subtitle-text mdl-card--expand">
-                  Today's Best Music
                 </div>
-              </div>
-            </div>
-            <div class="mdl-card mdl-shadow--2dp">
-              <div class="mdl-card__title mdl-card--expand">
-                <h2 class="mdl-card__title-text">
-                    Today's Best Music
-                  </h2>
-              </div>
-              <div class="mdl-card__title">
-                <div class="mdl-card__subtitle-text mdl-card--expand">
-                  Today's Best Music
+                <div class="mdl-card__title">
+                  <div class="mdl-card__subtitle-text">Lorem ipsum dolor sit amet</div>
                 </div>
-              </div>
-            </div>
-            <div class="mdl-card mdl-shadow--2dp">
-              <div class="mdl-card__title mdl-card--expand">
-                <h2 class="mdl-card__title-text">
-                    Today's Best Music
-                  </h2>
-              </div>
-              <div class="mdl-card__title">
-                <div class="mdl-card__subtitle-text mdl-card--expand">
-                  Today's Best Music
-                </div>
-              </div>
-            </div>
-            <div class="mdl-card mdl-shadow--2dp">
-              <div class="mdl-card__title mdl-card--expand">
-                <h2 class="mdl-card__title-text">
-                    Today's Best Music
-                  </h2>
-              </div>
-              <div class="mdl-card__title">
-                <div class="mdl-card__subtitle-text mdl-card--expand">
-                  Today's Best Music
-                </div>
-              </div>
-            </div>
-            <div class="mdl-card mdl-shadow--2dp">
-              <div class="mdl-card__title mdl-card--expand">
-                <h2 class="mdl-card__title-text">
-                    Today's Best Music
-                  </h2>
-              </div>
-              <div class="mdl-card__title">
-                <div class="mdl-card__subtitle-text mdl-card--expand">
-                  Today's Best Music
-                </div>
-              </div>
-            </div>
-            <div class="mdl-card mdl-shadow--2dp">
-              <div class="mdl-card__title mdl-card--expand">
-                <h2 class="mdl-card__title-text">
-                    Today's Best Music
-                  </h2>
-              </div>
-              <div class="mdl-card__title">
-                <div class="mdl-card__subtitle-text mdl-card--expand">
-                  Today's Best Music
-                </div>
-              </div>
-            </div>
-            <div class="mdl-card mdl-shadow--2dp">
-              <div class="mdl-card__title mdl-card--expand">
-                <h2 class="mdl-card__title-text">
-                    Today's Best Music
-                  </h2>
-              </div>
-              <div class="mdl-card__title">
-                <div class="mdl-card__subtitle-text mdl-card--expand">
-                  Today's Best Music
-                </div>
-              </div>
-            </div>
-            <div class="mdl-card mdl-shadow--2dp">
-              <div class="mdl-card__title mdl-card--expand">
-                <h2 class="mdl-card__title-text">
-                    Today's Best Music
-                  </h2>
-              </div>
-              <div class="mdl-card__title">
-                <div class="mdl-card__subtitle-text mdl-card--expand">
-                  Today's Best Music
-                </div>
-              </div>
-            </div>
-            <div class="mdl-card mdl-shadow--2dp">
-              <div class="mdl-card__title mdl-card--expand">
-                <h2 class="mdl-card__title-text">
-                    Today's Best Music
-                  </h2>
-              </div>
-              <div class="mdl-card__title">
-                <div class="mdl-card__subtitle-text mdl-card--expand">
-                  Today's Best Music
-                </div>
-              </div>
-            </div>
-            <div class="mdl-card mdl-shadow--2dp">
-              <div class="mdl-card__title mdl-card--expand">
-                <h2 class="mdl-card__title-text">
-                    Today's Best Music
-                  </h2>
-              </div>
-              <div class="mdl-card__title">
-                <div class="mdl-card__subtitle-text mdl-card--expand">
-                  Today's Best Music
+                <div class="mdl-card__play">
+                  <button class="mdl-button mdl-button--fab mdl-button--primary mdl-button--icon mdl-js-button mdl-js-ripple-effect"><i class="material-icons">play_arrow</i></button>
                 </div>
               </div>
             </div>
@@ -158,6 +53,7 @@ export default {
 
 <style lang="scss" scoped>
 @import "~@/assets/variables";
+@import "~material-design-lite/src/mixins";
 
 .iscroll-wrapper {
   position: relative;
@@ -172,14 +68,56 @@ export default {
   display: flex;
 }
 
-.mdl-card {
-  flex: 0 0 auto;
-  width: $music-set-card-width;
-  height: $music-set-card-height;
+.music-set-card {
   margin-right: 16px;
+  border: 2px;
+
+  background: url('~@/assets/album1.jpg');
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+
+  transition: box-shadow 0.28s cubic-bezier(0.4, 0, 0.2, 1);
+
+  &:hover {
+    @include focus-shadow();
+
+    .mdl-card__play {
+      visibility: visible;
+      opacity: 1;
+    }
+  }
+
+  .mdl-card {
+    flex: 0 0 auto;
+    width: $music-set-card-width;
+    height: $music-set-card-height;
+
+    background-image: linear-gradient(to top, rgb(0, 0, 0), rgba(0, 0, 0, 0.5));
+    background-color: transparent;
+  }
+
+  .mdl-card__title-text {
+    color: #fff;
+  }
+
+  .mdl-card__subtitle-text {
+    color: rgba(255, 255, 255, .5);
+  }
+
+  .mdl-card__play {
+    transition: all 0.28s cubic-bezier(0.4, 0, 0.2, 1);
+    visibility: hidden;
+    opacity: 0;
+
+    position: absolute;
+    right: 16px;
+    bottom: 16px;
+  }
+
+  &:last-of-type {
+    margin-right: 0;
+  }
 }
 
-.mdl-card:last-of-type {
-  margin-right: 0;
-}
 </style>
