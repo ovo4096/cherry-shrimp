8f4270a95025dfedbc8b56af1c22bd253932e75b
ovo4096 <ovo4096@gmail.com>
2017-04-15 11:10:55 +0800

    Update home layout

--- src/renderer/components/pages/Home.vue
+++ src/renderer/components/pages/Home.vue
@@ -1,107 +1,186 @@
 <template>
   <div class="page-content">
-    <div class="mdl-grid">
-      <div class="mdl-cell mdl-cell--12-col">
-        <h5 class="no-margin">Recommended for You</h5>
+    <template v-for="group in groups">
+      <div class="mdl-grid">
+        <div class="mdl-cell mdl-cell--12-col">
+          <div class="mdl-typography--title">{{ group.name }}</div>
+        </div>
       </div>
-    </div>
-    <div class="mdl-grid">
-      <div class="mdl-cell mdl-cell--12-col">
-        <div class="mdl-card mdl-shadow--2dp"></div>
-        <div class="mdl-card mdl-shadow--2dp"></div>
-        <div class="mdl-card mdl-shadow--2dp"></div>
-        <div class="mdl-card mdl-shadow--2dp"></div>
-        <div class="mdl-card mdl-shadow--2dp"></div>
-        <div class="mdl-card mdl-shadow--2dp"></div>
-        <div class="mdl-card mdl-shadow--2dp"></div>
-        <div class="mdl-card mdl-shadow--2dp"></div>
+      <div class="mdl-grid">
+        <div class="mdl-cell mdl-cell--12-col" ref="scrollbars">
+          <div class="mdl-card-group iscroll-wrapper">
+            <div class="mdl-card mdl-shadow--2dp">
+              <div class="mdl-card__title mdl-card--expand">
+                <h2 class="mdl-card__title-text">
+                    Today's Best Music
+                  </h2>
+              </div>
+              <div class="mdl-card__title">
+                <div class="mdl-card__subtitle-text mdl-card--expand">
+                  Today's Best Music
+                </div>
+              </div>
+            </div>
+            <div class="mdl-card mdl-shadow--2dp">
+              <div class="mdl-card__title mdl-card--expand">
+                <h2 class="mdl-card__title-text">
+                    Today's Best Music
+                  </h2>
+              </div>
+              <div class="mdl-card__title">
+                <div class="mdl-card__subtitle-text mdl-card--expand">
+                  Today's Best Music
+                </div>
+              </div>
+            </div>
+            <div class="mdl-card mdl-shadow--2dp">
+              <div class="mdl-card__title mdl-card--expand">
+                <h2 class="mdl-card__title-text">
+                    Today's Best Music
+                  </h2>
+              </div>
+              <div class="mdl-card__title">
+                <div class="mdl-card__subtitle-text mdl-card--expand">
+                  Today's Best Music
+                </div>
+              </div>
+            </div>
+            <div class="mdl-card mdl-shadow--2dp">
+              <div class="mdl-card__title mdl-card--expand">
+                <h2 class="mdl-card__title-text">
+                    Today's Best Music
+                  </h2>
+              </div>
+              <div class="mdl-card__title">
+                <div class="mdl-card__subtitle-text mdl-card--expand">
+                  Today's Best Music
+                </div>
+              </div>
+            </div>
+            <div class="mdl-card mdl-shadow--2dp">
+              <div class="mdl-card__title mdl-card--expand">
+                <h2 class="mdl-card__title-text">
+                    Today's Best Music
+                  </h2>
+              </div>
+              <div class="mdl-card__title">
+                <div class="mdl-card__subtitle-text mdl-card--expand">
+                  Today's Best Music
+                </div>
+              </div>
+            </div>
+            <div class="mdl-card mdl-shadow--2dp">
+              <div class="mdl-card__title mdl-card--expand">
+                <h2 class="mdl-card__title-text">
+                    Today's Best Music
+                  </h2>
+              </div>
+              <div class="mdl-card__title">
+                <div class="mdl-card__subtitle-text mdl-card--expand">
+                  Today's Best Music
+                </div>
+              </div>
+            </div>
+            <div class="mdl-card mdl-shadow--2dp">
+              <div class="mdl-card__title mdl-card--expand">
+                <h2 class="mdl-card__title-text">
+                    Today's Best Music
+                  </h2>
+              </div>
+              <div class="mdl-card__title">
+                <div class="mdl-card__subtitle-text mdl-card--expand">
+                  Today's Best Music
+                </div>
+              </div>
+            </div>
+            <div class="mdl-card mdl-shadow--2dp">
+              <div class="mdl-card__title mdl-card--expand">
+                <h2 class="mdl-card__title-text">
+                    Today's Best Music
+                  </h2>
+              </div>
+              <div class="mdl-card__title">
+                <div class="mdl-card__subtitle-text mdl-card--expand">
+                  Today's Best Music
+                </div>
+              </div>
+            </div>
+            <div class="mdl-card mdl-shadow--2dp">
+              <div class="mdl-card__title mdl-card--expand">
+                <h2 class="mdl-card__title-text">
+                    Today's Best Music
+                  </h2>
+              </div>
+              <div class="mdl-card__title">
+                <div class="mdl-card__subtitle-text mdl-card--expand">
+                  Today's Best Music
+                </div>
+              </div>
+            </div>
+            <div class="mdl-card mdl-shadow--2dp">
+              <div class="mdl-card__title mdl-card--expand">
+                <h2 class="mdl-card__title-text">
+                    Today's Best Music
+                  </h2>
+              </div>
+              <div class="mdl-card__title">
+                <div class="mdl-card__subtitle-text mdl-card--expand">
+                  Today's Best Music
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    <div class="mdl-grid">
-      <div class="mdl-cell mdl-cell--12-col">
-        <h5 class="no-margin">New releases</h5>
-      </div>
-    </div>
-    <div class="mdl-grid">
-      <div class="mdl-cell mdl-cell--12-col">
-        <div class="mdl-card mdl-shadow--2dp"></div>
-        <div class="mdl-card mdl-shadow--2dp"></div>
-        <div class="mdl-card mdl-shadow--2dp"></div>
-        <div class="mdl-card mdl-shadow--2dp"></div>
-        <div class="mdl-card mdl-shadow--2dp"></div>
-        <div class="mdl-card mdl-shadow--2dp"></div>
-        <div class="mdl-card mdl-shadow--2dp"></div>
-        <div class="mdl-card mdl-shadow--2dp"></div>
-      </div>
-    </div>
-    <div class="mdl-grid">
-      <div class="mdl-cell mdl-cell--12-col">
-        <h5 class="no-margin">Subscriptions</h5>
-      </div>
-    </div>
-    <div class="mdl-grid">
-      <div class="mdl-cell mdl-cell--12-col">
-        <div class="mdl-card mdl-shadow--2dp"></div>
-        <div class="mdl-card mdl-shadow--2dp"></div>
-        <div class="mdl-card mdl-shadow--2dp"></div>
-        <div class="mdl-card mdl-shadow--2dp"></div>
-        <div class="mdl-card mdl-shadow--2dp"></div>
-        <div class="mdl-card mdl-shadow--2dp"></div>
-        <div class="mdl-card mdl-shadow--2dp"></div>
-        <div class="mdl-card mdl-shadow--2dp"></div>
-      </div>
-    </div>
+    </template>
   </div>
 </template>
 
 <script>
-// import IScroll from 'iscroll'
+import IScroll from 'iscroll'
 
 export default {
-  // mounted () {
-  //   this._scrollbar = []
-  //   this._scrollbar.push(new IScroll(this.$refs.recommendRollbar, { scrollX: true, scrollY: false }))
-  //   this._scrollbar.push(new IScroll(this.$refs.newRollbar, { scrollX: true, scrollY: false }))
-  //   this._scrollbar.push(new IScroll(this.$refs.subscriptionRollbar, { scrollX: true, scrollY: false }))
-  // }
+  data: () => ({
+    groups: [
+      { name: 'Recommended for You' },
+      { name: 'New releases' },
+      { name: 'Subscriptions' }
+    ]
+  }),
+  mounted () {
+    this._scrollbars = []
+    for (let scrollbar of this.$refs.scrollbars) {
+      this._scrollbars.push(new IScroll(scrollbar, { scrollX: true, scrollY: false }))
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 @import "~@/assets/variables";
 
-.album-card.mdl-card {
-  width: $roll-card-height;
-  height: $roll-card-height;
-  background: $layout-header-bg-color;
-  margin-right: 8px;
-  float: left;
-}
-
-.album-card.mdl-card:last-of-type {
-  margin-right: 0;
-}
-
-.roll {
+.iscroll-wrapper {
   position: relative;
-  height: $roll-card-height + 16px;
-}
-
-.roll-container {
-  top: 8px;
-  left: 8px;
-  position: absolute;
-  z-index: 1;
-  width: 2136px;
-  transform: translateZ(0);
-  user-select: none;
-}
-
-.no-margin {
-  margin: 0;
+  width: 3000px;
 }
 
 .page-content {
   padding: 24px 0;
+}
+
+.mdl-card-group {
+  display: flex;
+}
+
+.mdl-card {
+  min-width: 256px;
+  min-height: 256px;
+  max-width: 256px;
+  max-height: 256px;
+  margin-right: 16px;
+}
+
+.mdl-card:last-of-type {
+  margin-right: 0;
 }
 </style>
