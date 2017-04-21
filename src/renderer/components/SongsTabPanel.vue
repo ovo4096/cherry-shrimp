cc5268536270a5ca7eebd2ac1fb0ea17ed484b51
ovo4096 <ovo4096@gmail.com>
2017-04-21 12:17:48 +0800

    Modify songs tab panel layout

--- src/renderer/components/SongsTabPanel.vue
+++ src/renderer/components/SongsTabPanel.vue
@@ -5,42 +5,41 @@
         <table class="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp">
           <thead>
             <tr>
-              <th class="mdl-data-table__cell--non-numeric min-cell">COVER</th>
-              <th class="mdl-data-table__cell--non-numeric">NAME</th>
+              <th class="mdl-data-table__cell--non-numeric min-cell">NAME</th>
+              <th class="mdl-data-table__cell--non-numeric"></th>
               <th class="mdl-data-table__cell--non-numeric">ARTIST</th>
               <th class="mdl-data-table__cell--non-numeric">ALBUM</th>
-              <th class="min-cell"></th>
-              <th class="min-cell"></th>
               <th class="min-cell"></th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="item in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]">
-              <td class="mdl-data-table__cell--non-numeric"><img class="music-cover" src="~@/assets/album1.jpg" alt="cover"></td>
+              <td class="mdl-data-table__cell--non-numeric">
+                <div class="music-cover">
+                  <img src="~@/assets/album1.jpg" alt="cover">
+                  <mdl-template class="music-play-button">
+                    <button class="mdl-button mdl-js-button mdl-button--icon">
+                      <i class="material-icons">play_circle_filled</i>
+                    </button>
+                  </mdl-template>
+                </div>
+              </td>
               <td class="mdl-data-table__cell--non-numeric">Futurism Love (feat. Miku Hatsune)</td>
               <td class="mdl-data-table__cell--non-numeric"><a href="">BAKAEDITZ</a></td>
               <td class="mdl-data-table__cell--non-numeric"><a href="">Life in the 2k12</a></td>
               <td>
-                <mdl-template>
-                  <label class="mdl-icon-toggle mdl-js-icon-toggle mdl-js-ripple-effect">
-                    <input type="checkbox" class="mdl-icon-toggle__input" checked>
-                    <i class="mdl-icon-toggle__label material-icons">favorite</i>
-                  </label>
-                </mdl-template>
-              </td>
-              <td>
-                <mdl-template>
-                  <button class="mdl-button mdl-js-button mdl-button--icon">
-                    <i class="material-icons">play_arrow</i>
-                  </button>
-                </mdl-template>
-              </td>
-              <td>
-                <mdl-template>
-                  <button class="mdl-button mdl-js-button mdl-button--icon">
-                    <i class="material-icons">file_download</i>
-                  </button>
-                </mdl-template>
+                <div class="button-group">
+                  <mdl-template>
+                    <button class="mdl-button mdl-js-button mdl-button--icon">
+                      <i class="material-icons">delete_favorite</i>
+                    </button>
+                  </mdl-template>
+                  <mdl-template>
+                    <button class="mdl-button mdl-js-button mdl-button--icon">
+                      <i class="material-icons">file_download</i>
+                    </button>
+                  </mdl-template>
+                </div>
               </td>
             </tr>
           </tbody>
@@ -66,18 +65,56 @@ export default {
 .mdl-data-table th:first-of-type {
   width: 1px;
 }
+
+.mdl-data-table td {
+  border-bottom: none;
+}
 </style>
 
 <style lang="scss" scoped>
+@import "~@/assets/variables";
+
 .songs-tab-panel {
   .page-content {
-    margin: 32px 16px;
+    padding: 32px 16px;
+    background: $global-bg-color;
   }
 
   .music-cover {
-    display: block;
     width: 48px;
     height: 48px;
+    position: relative;
+
+    img {
+      display: block;
+      width: 100%;
+      height: 100%;
+    }
+
+    .music-play-button {
+      visibility: hidden;
+      opacity: 0;
+      transition-duration: 0.28s;
+      transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+      transition-property: opacity;
+      position: absolute;
+      left: 0;
+      top: 0;
+      height: 100%;
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background: rgba(0, 0, 0, 0.25);
+      .material-icons {
+        color: #fff;
+      }
+    }
+  }
+
+  .mdl-data-table tr:hover .music-cover .music-play-button {
+    visibility: visible;
+    opacity: 1;
   }
 
   .min-cell {
@@ -87,7 +124,6 @@ export default {
   .mdl-data-table {
     width: 100%;
     border-radius: 2px;
-    overflow: hidden;
     border: none;
   }
 
