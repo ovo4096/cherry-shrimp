6579e50b7a32390072c0f9901b1b2b763c56f22c
ovo4096 <ovo4096@gmail.com>
2017-04-23 11:15:09 +0800

    Add album card menu

--- src/renderer/components/AlbumsTabPanel.vue
+++ src/renderer/components/AlbumsTabPanel.vue
@@ -2,19 +2,32 @@
   <section class="mdl-layout__tab-panel albums-tab-panel">
     <div class="page-content">
       <div class="flow-grid">
-        <div v-for="item in new Array(100)" class="album-card mdl-card mdl-shadow--2dp">
+        <div v-for="(item, index) in new Array(50)" class="album-card mdl-card mdl-shadow--2dp">
           <div class="mdl-card__media">
             <img src="~@/assets/album1.jpg">
             <mdl-template class="album-card__play-button">
-              <button class="mdl-button mdl-button--fab mdl-button--primary mdl-button--icon mdl-js-button mdl-js-ripple-effect"><i class="material-icons">play_arrow</i></button>
+              <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect"><i class="material-icons">play_arrow</i></button>
             </mdl-template>
           </div>
           <div class="mdl-card__title">
-            <a href="#" class="album-card__title-text">Guardians</a>
-            <a href="#" class="album-card__subtitle-text">Life in the 2k12</a>
+            <div class="album-card__title-text">Guardians</div>
+            <div class="album-card__subtitle-text">Life in the 2k12</div>
+            <mdl-template class="album-card__menu-button">
+              <button :id="'album-card-menu-' + index" class="mdl-button mdl-js-button mdl-button--icon">
+                <i class="material-icons">more_vert</i>
+              </button>
+              <mdl-template>
+                <ul class="mdl-menu mdl-menu--top-right mdl-js-menu mdl-js-ripple-effect" :for="'album-card-menu-' + index">
+                  <li class="mdl-menu__item">Some Action</li>
+                  <li class="mdl-menu__item mdl-menu__item--full-bleed-divider">Another Action</li>
+                  <li disabled class="mdl-menu__item">Disabled Action</li>
+                  <li class="mdl-menu__item">Yet Another Action</li>
+                </ul>
+              </mdl-template>
+            </mdl-template>
           </div>
         </div>
-        <div v-for="item in new Array(99)" class="album-card-spacer"></div>
+        <div v-for="item in new Array(49)" class="album-card-spacer"></div>
       </div>
     </div>
   </section>
@@ -53,43 +66,94 @@ export default {
     flex-grow: 1;
   }
   .album-card.mdl-card {
+    cursor: pointer;
     width: 200px;
     min-height: auto;
     margin: 8px;
     flex-grow: 1;
     flex-shrink: 1;
+    transition: box-shadow .25s ease-in-out;
+
+    &:hover {
+      @include focus-shadow();
+    }
+
     .mdl-card__media {
       img {
         width: 100%;
       }
       position: relative;
-
-      &:hover .album-card__play-button {
-        opacity: 1;
-      }
     }
+
     .album-card__play-button {
       position: absolute;
       bottom: 8px;
       right: 8px;
+      visibility: hidden;
       opacity: 0;
       transition: opacity .25s ease-in-out;
-    } 
+
+      .mdl-button {
+        height: 40px;
+        width: 40px;
+        min-width: 40px;
+        font-size: 26px;
+        background-color: rgba(0, 0, 0, .3);
+        &:hover {
+          background-color: rgba(0, 0, 0, .5);
+        }
+
+        .material-icons {
+          font-size: 26px;
+          transform: translate(-13px, -13px);
+          line-height: 26px;
+          width: 26px;
+          color: #fff;
+        }
+      }
+    }
+    &:hover .album-card__play-button {
+      visibility: visible;
+      opacity: 1;
+    }
+
+    .album-card__menu-button {
+      position: absolute;
+      right: 4px;
+      top: 8px;
+
+      .mdl-button {
+        height: 26px;
+        width: 26px;
+        min-width: 26px;
+        font-size: 18px;
+
+        .material-icons {
+          font-size: 18px;
+          transform: translate(-9px, -9px);
+          line-height: 18px;
+          width: 18px;
+          color: #333;
+        }
+      }
+    }
+
     .mdl-card__title {
+      position: relative;
       align-items: flex-start;
       flex-direction: column;
       .album-card__title-text {
+        width: 100%;
         @include typo-subhead();
         @include typo-hidden-overflow();
-        color: #000;
-        @include no-underline-link();
+        color: #222;
       }
       .album-card__subtitle-text {
+        width: 100%;
         margin-top: 4px;
         @include typo-caption();
         @include typo-hidden-overflow();
         color: rgba(0, 0, 0, 0.54);
-        @include no-underline-link();
       }
     }
   }
